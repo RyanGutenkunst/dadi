6825e4579560285d1284b11678832b299fbc2dcf
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2008-06-03 17:18:50 +0000

    Initial commit of Demographics1D.py
    git-svn-id: https://dadi.googlecode.com/svn/trunk@26 979d6bd5-6d4d-0410-bece-f567c23bd345

---
+++ dadi/Demographics1D.py
@@ -0,0 +1,77 @@
+"""
+Single population demographic models.
+"""
+import numpy
+
+from dadi import Numerics, PhiManip, SFS, Integration
+
+def snm(n1, pts):
+    """
+    Standard neutral model.
+
+    n1: Number of samples in resulting SFS
+    pts: Number of grid points to use in integration.
+    """
+    xx = Numerics.other_grid(pts)
+    phi = PhiManip.phi_1D(xx)
+
+    sfs = SFS.sfs_from_phi_1D(n1, xx, phi)
+    return sfs
+
+def two_epoch((nu, T), n1, pts):
+    """
+    Instantaneous size change some time ago.
+
+    nu: Ratio of contemporary to ancient population size
+    T: Time in the past at which size change happened (in units of 2*Na 
+       generations) 
+    n1: Number of samples in resulting SFS
+    pts: Number of grid points to use in integration.
+    """
+    xx = Numerics.other_grid(pts)
+    phi = PhiManip.phi_1D(xx)
+    
+    phi = Integration.one_pop(phi, xx, T, nu)
+
+    sfs = SFS.sfs_from_phi_1D(n1, xx, phi)
+    return sfs
+
+def growth((nu, T), n1, pts):
+    """
+    Exponential growth beginning some time ago.
+
+    nu: Ratio of contemporary to ancient population size
+    T: Time in the past at which growth began (in units of 2*Na 
+       generations) 
+    n1: Number of samples in resulting SFS
+    pts: Number of grid points to use in integration.
+    """
+    xx = Numerics.other_grid(pts)
+    phi = PhiManip.phi_1D(xx)
+
+    nu_func = lambda t: numpy.exp(numpy.log(nu) * t/T)
+    phi = Integration.one_pop(phi, xx, T, nu_func)
+
+    sfs = SFS.sfs_from_phi_1D(n1, xx, phi)
+    return sfs
+
+def bottlegrowth((nuB, nuF, T), n1, pts):
+    """
+    Instantanous size change followed by exponential growth.
+
+    nuB: Ratio of population size after instantanous change to ancient
+         population size
+    nuF: Ratio of contempoary to ancient population size
+    T: Time in the past at which instantaneous change happened and growth began
+       (in units of 2*Na generations) 
+    n1: Number of samples in resulting SFS
+    pts: Number of grid points to use in integration.
+    """
+    xx = Numerics.other_grid(pts)
+    phi = PhiManip.phi_1D(xx)
+
+    nu_func = lambda t: nuB*numpy.exp(numpy.log(nuF/nuB) * t/T)
+    phi = Integration.one_pop(phi, xx, T, nu_func)
+
+    sfs = SFS.sfs_from_phi_1D(n1, xx, phi)
+    return sfs
