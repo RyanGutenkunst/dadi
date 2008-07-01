e35952e4ac11da2c41760985ed2b09079b63e1be
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2008-07-01 18:21:47 +0000

    Added randomly_sampled_2D sfs method, which is useful for measuring degree of divergence.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@58 979d6bd5-6d4d-0410-bece-f567c23bd345

--- dadi/SFS.py
+++ dadi/SFS.py
@@ -467,3 +467,38 @@ def Fst(sfs):
     dsum = (sfs * d).sum()
 
     return asum/(asum+dsum)
+
+def randomly_resampled_2D(sfs):
+    """
+    Randomly scramble individuals among the populations. 
+    
+    This is useful for measuring divergence. Essentially, this method pools all
+    the individuals represented in the sfs and generates two new populations of
+    random individuals (without replacement) from that pool.
+    """
+    n1,n2 = numpy.asarray(sfs.shape)-1
+    ntot = n1+n2
+
+    # First generate a 1d sfs for the pooled population.
+    sfs_combined = numpy.zeros(ntot+1)
+    for ii, row in enumerate(sfs):
+        for jj, num_snps in enumerate(row):
+            derived_total = ii+jj
+            # This isscalar check deals with masking
+            if numpy.isscalar(num_snps):
+                sfs_combined[derived_total] += num_snps
+
+    # Now resample
+    sfs_resamp = numpy.zeros(sfs.shape)
+    for derived1 in range(n1+1):
+        for derived_total, num_snps in enumerate(sfs_combined):
+            derived2 = derived_total - derived1
+            ancestral_total = ntot - derived_total
+            ancestral1 = n1 - derived1
+
+            prob = numpy.exp(lncomb(derived_total, derived1)
+                             + lncomb(ancestral_total, ancestral1)
+                             - lncomb(ntot, n1))
+            if prob > 0:
+                sfs_resamp[derived1, derived2] += prob*num_snps
+    return sfs_resamp
