cf3ec04887fdc829d27c7e141529bc543c83dd70
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2008-11-04 20:33:22 +0000

    Move sfs scaling code to Inference.py, which leaves SFS.py empty.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@122 979d6bd5-6d4d-0410-bece-f567c23bd345

--- dadi/Inference.py
+++ dadi/Inference.py
@@ -217,3 +217,21 @@ def Anscombe_Poisson_residual(model, data, mask=None):
     # model is high, the residual is positive. This is opposite of the
     # Pierce and Schafner convention.
     return -resid
+
+def optimally_scaled_sfs(model, data):
+    """
+    Optimially scale model sfs to data sfs.
+
+    Returns a new scaled model sfs.
+    """
+    return optimal_sfs_scaling(model,data) * model
+
+def optimal_sfs_scaling(model, data):
+    """
+    Optimal multiplicative scaling factor between model and data.
+
+    This scaling is based on only those entries that are masked in neither
+    model nor data.
+    """
+    model, data = Numerics.intersect_masks(model, data)
+    return data.sum()/model.sum()
