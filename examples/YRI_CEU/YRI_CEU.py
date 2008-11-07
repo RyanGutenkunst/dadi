84cc77d12b37fa8c017aed1e4c3e980a37691659
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2008-11-07 03:05:41 +0000

    examples: Initial import of YRI_CEU example.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@157 979d6bd5-6d4d-0410-bece-f567c23bd345

---
+++ examples/YRI_CEU/YRI_CEU.py
@@ -0,0 +1,73 @@
+# Numpy is the numerical library dadi is built upon
+import numpy
+from numpy import array
+
+import dadi
+
+# In demographic_models.py, we've defined a custom model for this problem
+import demographic_models
+
+# Load the data
+data = dadi.Spectrum.from_file('YRI_CEU.fs')
+ns = data.sample_sizes
+
+# These are the grid point settings will use for extrapolation.
+pts_l = [40,50,60]
+
+# The Demographics1D and Demographics2D modules contain a few simple models,
+# mostly as examples. We could use one of those.
+func = dadi.Demographics2D.split_mig
+# ll for this model: -1136.61
+params = array([1.792, 0.426, 0.309, 1.551])
+upper_bound = [100, 100, 3, 20]
+
+# Instead, we'll work with our custom model
+func = demographic_models.prior_onegrow_mig
+# ll for this model: -1066.66
+params = array([1.878, 0.0740, 1.783, 0.925, 0.353, 0.112])
+# The upper_bound array is for use in optimization. Occasionally the optimizer
+# will try wacky parameter values. We in particular want to exclude values with
+# very long times, as they will take a long time to evaluate.
+upper_bound = [100, 100, 100, 100, 3, 3]
+
+# Makde the extrapolating version of our demographic model function.
+func_ex = dadi.Numerics.make_extrap_func(func)
+# Calculate the model AFS.
+model = func_ex(params, ns, pts_l)
+# Likelihood of the data given the model AFS.
+ll_model = dadi.Inference.ll_multinom(model, data)
+print 'Model log-likelihood:', ll_model
+# The optimal value of theta given the model.
+theta = dadi.Inference.optimal_sfs_scaling(model, data)
+
+# Perturb our parameter array before optimization. This does so by taking each
+# parameter a up to a factor of two up or down.
+p0 = dadi.Misc.perturb_params(params, fold=1, upper_bound=upper_bound)
+# Do the optimization. By default we assume that theta is a free parameter,
+# since it's trivial to find given the other parameters. If you want to fix
+# theta, add a multinom=False to the call.
+popt = dadi.Inference.optimize_log(p0, data, func_ex,
+                                   pts_l, upper_bound=upper_bound,
+                                   verbose=len(params))
+print 'Optimized parameters', repr(popt)
+model = func_ex(popt, ns, pts_l)
+ll_opt = dadi.Inference.ll_multinom(model, data)
+print 'Optimized log-likelihood:', ll_opt
+
+# Plot a comparison of the resulting fs.
+dadi.Plotting.figure()
+dadi.Plotting.plot_2d_comp_multinom(model, data, vmin=1, resid_range=3)
+dadi.Plotting.show()
+
+# Let's generate some data using ms, if you have it installed.
+mscore = demographic_models.prior_onegrow_mig_mscore(params)
+# I find that it's most efficient to simulate with theta=1 and then scale up.
+mscommand = dadi.Misc.ms_command(1., ns, mscore, int(1e6))
+# We use Python's os module to call this command from within the script.
+# If you have ms installed, uncomment these lines to see the results.
+#import os
+#os.system('%s > test.msout' % mscommand)
+#msdata = dadi.Spectrum.from_ms_file('test.msout')
+#dadi.Plotting.figure()
+#dadi.Plotting.plot_2d_comp_multinom(model, theta*msdata, vmin=1)
+#dadi.show()
