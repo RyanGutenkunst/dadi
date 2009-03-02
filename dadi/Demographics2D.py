ed9f016c2c306ce05d1a2a88c36bd8fdf73be5d4
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2009-03-02 21:14:07 +0000

    Demographics2D: Added IM model.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@234 979d6bd5-6d4d-0410-bece-f567c23bd345

--- dadi/Demographics2D.py
+++ dadi/Demographics2D.py
@@ -107,3 +107,29 @@ def split_mig_mscore((nu1, nu2, T, m)):
     sub_dict = {'nu1':nu1, 'nu2':nu2, 'm12':2*m, 'm21':2*m, 'T': T/2}
 
     return command % sub_dict
+
+def IM((s, nu1, nu2, T, m12, m21), (n1,n2), pts):
+    """
+    Isolation-with-migration model with exponential pop growth.
+
+    s: Size of pop 1 after split. (Pop 2 has size 1-s.)
+    nu1: Final size of pop 1.
+    nu2: Final size of pop 2.
+    T: Time in the past of split (in units of 2*Na generations) 
+    m12: Migration from pop 2 to pop 1 (2*Na*m12)
+    m21: Migration from pop 1 to pop 2
+    n1,n2: Shape of resulting SFS
+    pts: Number of grid points to use in integration.
+    """
+    xx = Numerics.default_grid(pts)
+
+    phi = PhiManip.phi_1D(xx)
+    phi = PhiManip.phi_1D_to_2D(xx, phi)
+
+    nu1_func = lambda t: s * (nu1/s)**(T/t)
+    nu2_func = lambda t: (1-s) * (nu2/(1-s))**(t/T)
+    phi = Integration.two_pops(phi, xx, T, nu1_func, nu2_func,
+                               m12=m12, m21=m21)
+
+    sfs = Spectrum.from_phi(phi, (n1,n2), (xx,xx))
+    return sfs
