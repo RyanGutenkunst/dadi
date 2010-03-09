a72c2826b11b8c2d9b3561975441e72f8f97f483
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2010-03-09 23:12:26 +0000

    Restore YRI_CEU.py example to skip optimization.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@344 979d6bd5-6d4d-0410-bece-f567c23bd345

--- examples/YRI_CEU/YRI_CEU.py
+++ examples/YRI_CEU/YRI_CEU.py
@@ -44,23 +44,21 @@ print 'Model log-likelihood:', ll_model
 # The optimal value of theta given the model.
 theta = dadi.Inference.optimal_sfs_scaling(model, data)
 
-# Perturb our parameter array before optimization. This does so by taking each
-# parameter a up to a factor of two up or down.
-p0 = dadi.Misc.perturb_params(params, fold=1, upper_bound=upper_bound)
-# Do the optimization. By default we assume that theta is a free parameter,
-# since it's trivial to find given the other parameters. If you want to fix
-# theta, add a multinom=False to the call.
-# (This is commented out by default, since it takes several minutes.)
-popt = dadi.Inference.optimize_log(p0, data, func_ex,
-                                   pts_l, 
-                                   fixed_params = array([None, 0.0710, None, 0.911, 0.355, 0.111]),
-                                   lower_bound=lower_bound,
-                                   upper_bound=upper_bound,
-                                   verbose=len(params))
-#print 'Optimized parameters', repr(popt)
-model = func_ex(popt, ns, pts_l)
-ll_opt = dadi.Inference.ll_multinom(model, data)
-print 'Optimized log-likelihood:', ll_opt
+## Perturb our parameter array before optimization. This does so by taking each
+## parameter a up to a factor of two up or down.
+#p0 = dadi.Misc.perturb_params(params, fold=1, upper_bound=upper_bound)
+## Do the optimization. By default we assume that theta is a free parameter,
+## since it's trivial to find given the other parameters. If you want to fix
+## theta, add a multinom=False to the call.
+## (This is commented out by default, since it takes several minutes.)
+#popt = dadi.Inference.optimize_log(p0, data, func_ex, pts_l, 
+#                                   lower_bound=lower_bound,
+#                                   upper_bound=upper_bound,
+#                                   verbose=len(params))
+##print 'Optimized parameters', repr(popt)
+#model = func_ex(popt, ns, pts_l)
+#ll_opt = dadi.Inference.ll_multinom(model, data)
+#print 'Optimized log-likelihood:', ll_opt
 
 # Plot a comparison of the resulting fs with the data.
 import pylab
