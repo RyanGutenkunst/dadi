14e1fa7d504e663fffd2846a3a9a89da2458ff37
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2010-04-20 15:56:23 +0000

    setup.py: If using gcc, warn if using non-C89 features.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@355 979d6bd5-6d4d-0410-bece-f567c23bd345

--- setup.py
+++ setup.py
@@ -6,20 +6,39 @@ try:
 except ImportError:
     pass
 
-import os
+import os,sys
 
 import numpy.distutils.core as core
 
+#
+# Microsoft Visual C++ only supports C up to the version iso9899:1990 (C89).
+# gcc by default supports much more. To ensure MSVC++ compatibility when using
+# gcc, we need to add extra compiler args. This code tries to ensure such
+# arguments are added *only* when we're using gcc.
+#
+import numpy.distutils
+compiler = numpy.distutils.ccompiler.get_default_compiler()
+for arg in sys.argv:
+    if arg.startswith('--compiler'):
+        compiler = arg.split('=')[1]
+if compiler in ['unix','mingw32','cygwin']:
+    extra_compile_args = ['-std="iso9899:1990"', '-pedantic']
+else:
+    extra_compile_args = []
+
+
 # Configure our C modules that are built with f2py.
 tridiag = core.Extension(name = 'dadi.tridiag',
-                         sources = ['dadi/tridiag.pyf', 'dadi/tridiag.c'])
+                         sources = ['dadi/tridiag.pyf', 'dadi/tridiag.c'],
+                         extra_compile_args=extra_compile_args)
 int_c = core.Extension(name = 'dadi.integration_c',
                        sources = ['dadi/integration_c.pyf', 
                                   'dadi/integration1D.c',
                                   'dadi/integration2D.c', 
                                   'dadi/integration3D.c',
                                   'dadi/integration_shared.c',
-                                  'dadi/tridiag.c'])
+                                  'dadi/tridiag.c'],
+                         extra_compile_args=extra_compile_args)
 
 # If we're building a distribution, try to update svnversion. Note that this
 # fails silently.
