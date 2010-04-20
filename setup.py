08411a66440ca10afe54aba37606751ece90a67b
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2010-04-20 15:59:56 +0000

    Update setup.py and API doc for 1.4.1 release.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@357 979d6bd5-6d4d-0410-bece-f567c23bd345

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ for arg in os.sys.argv:
         os.system("svn info > dadi/svnversion")
 
 core.setup(name='dadi',
-           version='1.4.0',
+           version='1.4.1',
            author='Ryan Gutenkunst',
            author_email='ryan@gutenkunst.org',
            url='http://dadi.googlecode.com',
