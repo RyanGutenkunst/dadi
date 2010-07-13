27fdbb2c73c60f0ff65f0c83bc6765b7de6ad192
rgutenkunst <rgutenkunst@979d6bd5-6d4d-0410-bece-f567c23bd345>
2010-07-13 21:08:39 +0000

    Update setup.py to 1.5.0 version number.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@373 979d6bd5-6d4d-0410-bece-f567c23bd345

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ for arg in os.sys.argv:
         os.system("svn info > dadi/svnversion")
 
 core.setup(name='dadi',
-           version='1.4.1',
+           version='1.5.0',
            author='Ryan Gutenkunst',
            author_email='ryan@gutenkunst.org',
            url='http://dadi.googlecode.com',
