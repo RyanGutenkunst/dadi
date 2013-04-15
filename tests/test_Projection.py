7794500c352b0a2f536bbac7be07837d8fa0efef
rgutenkunst@gmail.com <rgutenkunst@gmail.com@979d6bd5-6d4d-0410-bece-f567c23bd345>
2013-04-15 21:29:46 +0000

    Add test for projecting parameters upward.
    git-svn-id: https://dadi.googlecode.com/svn/trunk@489 979d6bd5-6d4d-0410-bece-f567c23bd345

---
+++ tests/test_Projection.py
@@ -0,0 +1,23 @@
+import unittest
+import numpy
+import dadi
+
+class ProjectionTestCase(unittest.TestCase):
+    def test_project_up(self):
+        """
+        Saving spectrum to file.
+        """
+        fixed_params = [0.1,None,None]
+        params_up = dadi.Inference._project_params_up([0.2,0.3], fixed_params)
+        self.assertTrue(numpy.allclose(params_up, [0.1,0.2,0.3]))
+
+        fixed_params = [0.1,0.2,None]
+        params_up = dadi.Inference._project_params_up([0.3], fixed_params)
+        self.assertTrue(numpy.allclose(params_up, [0.1,0.2,0.3]))
+
+        fixed_params = [0.1,0.2,None]
+        params_up = dadi.Inference._project_params_up(0.3, fixed_params)
+        self.assertTrue(numpy.allclose(params_up, [0.1,0.2,0.3]))
+
+suite = unittest.TestLoader().loadTestsFromTestCase(ProjectionTestCase)
+unittest.main()
