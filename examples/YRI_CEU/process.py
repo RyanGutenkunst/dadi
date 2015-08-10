f683a8cb999eed49505315a57e446ed7cf484456
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2015-08-10 16:51:24 -0700

    Remove script for pulling bootstrap data into dadi-devel. We don't want to distribute it with the dadi code, and it was only temporarily needed.

--- examples/YRI_CEU/process.py
+++
@@ -1,7 +0,0 @@
-import glob
-import dadi
-
-for ii, fname in enumerate(glob.glob('/Users/rgutenk/Desktop/Documents/Research/2009/fit3/conventional_boot/*.fs')):
-    fsin = dadi.Spectrum.from_file(fname)
-    fsout = fsin.marginalize([2])
-    dadi.Spectrum.tofile(fsout, 'bootstraps/{0:02d}.fs'.format(ii))
