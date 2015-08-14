d8d3824f59289eb633699111b36106a07273961c
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2015-08-14 12:49:58 -0700

    Update YRI_CEU example for new code. Including extensive reworking to make example more complete and easier to follow.

--- examples/YRI_CEU/YRI_CEU.py
+++ examples/YRI_CEU/YRI_CEU.py
@@ -1,5 +1,4 @@
 # Numpy is the numerical library dadi is built upon
-import numpy
 from numpy import array
 
 import dadi
@@ -17,144 +16,146 @@ pts_l = [40,50,60]
 # The Demographics1D and Demographics2D modules contain a few simple models,
 # mostly as examples. We could use one of those.
 func = dadi.Demographics2D.split_mig
-# ll for this model: -1136.61
-params = array([1.792, 0.426, 0.309, 1.551])
-upper_bound = [100, 100, 3, 20]
-
 # Instead, we'll work with our custom model
 func = demographic_models.prior_onegrow_mig
-# ll for this model: -1066.66
-# These are the best-fit models parameters.
-params = array([1.881, 0.0710, 1.845, 0.911, 0.355, 0.111])
-# The upper_bound array is for use in optimization. Occasionally the optimizer
-# will try wacky parameter values. We in particular want to exclude values with
-# very long times, as they will take a long time to evaluate.
-upper_bound = [100, 100, 100, 100, 3, 3]
+
+# Now let's optimize parameters for this model.
+
+# The upper_bound and lower_bound lists are for use in optimization.
+# Occasionally the optimizer will try wacky parameter values. We in particular
+# want to exclude values with very long times, very small population sizes, or
+# very high migration rates, as they will take a long time to evaluate.
+# Parameters are: (nu1F, nu2B, nu2F, m, Tp, T)
+upper_bound = [100, 100, 100, 10, 3, 3]
 lower_bound = [1e-2, 1e-2, 1e-2, 0, 0, 0]
 
-# Makde the extrapolating version of our demographic model function.
+# This is our initial guess for the parameters, which is somewhat arbitrary.
+p0 = [2,0.1,2,1,0.2,0.2]
+# Make the extrapolating version of our demographic model function.
 func_ex = dadi.Numerics.make_extrap_log_func(func)
-# Calculate the model AFS.
-model = func_ex(params, ns, pts_l)
-# Likelihood of the data given the model AFS.
-ll_model = dadi.Inference.ll_multinom(model, data)
-print 'Model log-likelihood:', ll_model
-# The optimal value of theta given the model.
-theta = dadi.Inference.optimal_sfs_scaling(model, data)
 
-# Perturb our parameter array before optimization. This does so by taking each
+# Perturb our parameters before optimization. This does so by taking each
 # parameter a up to a factor of two up or down.
-p0 = dadi.Misc.perturb_params(params, fold=1, upper_bound=upper_bound)
+p0 = dadi.Misc.perturb_params(p0, fold=1, upper_bound=upper_bound,
+                              lower_bound=lower_bound)
 # Do the optimization. By default we assume that theta is a free parameter,
 # since it's trivial to find given the other parameters. If you want to fix
 # theta, add a multinom=False to the call.
-# (This is commented out by default, since it takes several minutes.)
 # The maxiter argument restricts how long the optimizer will run. For real 
-# runs, you will want to set this value higher, to encourage better
-# convergence.
+# runs, you will want to set this value higher (at least 10), to encourage
+# better convergence. You will also want to run optimization several times
+# using multiple sets of intial parameters, to be confident you've actually
+# found the true maximum likelihood parameters.
+print('Beginning optimization ************************************************')
 popt = dadi.Inference.optimize_log(p0, data, func_ex, pts_l, 
                                    lower_bound=lower_bound,
                                    upper_bound=upper_bound,
-                                   verbose=len(params),
-                                   maxiter=3)
+                                   verbose=len(p0), maxiter=3)
+# The verbose argument controls how often progress of the optimizer should be
+# printed. It's useful to keep track of optimization process.
+print('Finshed optimization **************************************************')
 
-print 'Optimized parameters', repr(popt)
+# These are the actual best-fit model parameters, which we found through
+# longer optimizations and confirmed by running multiple optimizations.
+# We'll work with them through the rest of this script.
+popt = [1.881, 0.0710, 1.845, 0.911, 0.355, 0.111]
+print('Best-fit parameters: {0}'.format(popt))
+
+# Calculate the best-fit model AFS.
 model = func_ex(popt, ns, pts_l)
-ll_opt = dadi.Inference.ll_multinom(model, data)
-print 'Optimized log-likelihood:', ll_opt
-# The optimal value of theta given the model and the optimized parameters.
-theta_opt = dadi.Inference.optimal_sfs_scaling(model, data)
-"""
+# Likelihood of the data given the model AFS.
+ll_model = dadi.Inference.ll_multinom(model, data)
+print('Maximum log composite likelihood: {0}'.format(ll_model))
+# The optimal value of theta given the model.
+theta = dadi.Inference.optimal_sfs_scaling(model, data)
+print('Optimal value of theta: {0}'.format(theta))
+
 # Plot a comparison of the resulting fs with the data.
 import pylab
-pylab.figure()
+pylab.figure(1)
 dadi.Plotting.plot_2d_comp_multinom(model, data, vmin=1, resid_range=3,
                                     pop_ids =('YRI','CEU'))
 # This ensures that the figure pops up. It may be unecessary if you are using
 # ipython.
 pylab.show()
+# Save the figure
 pylab.savefig('YRI_CEU.png', dpi=50)
-"""
+
 # Let's generate some data using ms, if you have it installed.
-mscore = demographic_models.prior_onegrow_mig_mscore(params)
-# I find that it's most efficient to simulate with theta=1 and then scale up.
-mscommand = dadi.Misc.ms_command(1., ns, mscore, int(1e6))
-## We use Python's os module to call this command from within the script.
-## If you have ms installed, uncomment these lines to see the results.
-#import os
-#os.system('%s > test.msout' % mscommand)
-#msdata = dadi.Spectrum.from_ms_file('test.msout')
-#pylab.figure()
-#dadi.Plotting.plot_2d_comp_multinom(model, theta*msdata, vmin=1,
-#                                    pop_ids=('YRI','CEU'))
-#pylab.show()
+mscore = demographic_models.prior_onegrow_mig_mscore(popt)
+# I find that it's most efficient to simulate with theta=1, average over many
+# iterations, and then scale up.
+mscommand = dadi.Misc.ms_command(1., ns, mscore, int(1e5))
+# If you have ms installed, uncomment these lines to see the results.
 
-# These are the optimal parameter estimates for this model and data, from long
-# optimization runs.
-popt = array([ 1.87913514,  0.07206629,  1.77945941,  0.92878784,  0.36389356,
-        0.11160405])
+# We use Python's os module to call this command from within the script.
+import os
+return_code = os.system('{0} > test.msout'.format(mscommand))
+# We check the return code, so the script doesn't crash if you don't have ms
+# installed
+if return_code == 0:
+    msdata = dadi.Spectrum.from_ms_file('test.msout')
+    pylab.figure(2)
+    dadi.Plotting.plot_2d_comp_multinom(model, theta*msdata, vmin=1,
+                                        pop_ids=('YRI','CEU'))
+    pylab.show()
 
-# Below here we compare uncertainty estimates from folded and unfolded spectra.
-# Estimates are done using the hessian (Fischer Information Matrix).
-# Due to linkage in the data, these are underestimates. It is still
-# informative, however, to compare the two methods.
-
-# These are the optimal parameters when the spectrum is folded. They can be
-# found simply by passing fold=True to the above call to optimize_log.
-pfold =  array([1.907,  0.073,  1.830,  0.899,  0.425,  0.113])
-
-# The interface to hessian computation is designed for general functions, so we
-# need to define the specific functions of interest here. These functions
-# calculate -ll given the logs of the parameters. (Because we work in log
-# parameters, the uncertainties we estimate will be *relative* parameter
-# uncertainties.)
-from dadi.Inference import ll_multinom
-func = lambda lp: -ll_multinom(func_ex(numpy.exp(lp), ns, pts_l), data)
-foldfunc = lambda lp: -ll_multinom(func_ex(numpy.exp(lp), ns, pts_l).fold(), 
-                                   data.fold()) 
-"""
-# Calculate the two hessians
-h = dadi.Hessian.hessian(func, numpy.log(popt), 0.05)
-hfold = dadi.Hessian.hessian(foldfunc, numpy.log(pfold), 0.05)
-
-# Now we calculate the *relative* parameter uncertainties.
-uncerts = numpy.sqrt(numpy.diag(numpy.linalg.inv(h)))
-uncerts_folded = numpy.sqrt(numpy.diag(numpy.linalg.inv(hfold)))
-
-# The increase in uncertainty is not too bad. Tp increasing by 50% is the only
-# substantial one.
-print uncerts_folded/uncerts - 1
-"""
-# Now we calculate uncertainty estimates using the Godambe Information Matrix,
-# which accounts for linkage in the data that the Hessian approach neglects.
-# To use the GIM approach, we need to have spectra from bootstrapping our data.
-# Let's load the ones we've provided for the example.
+# Estimate parameter uncertainties using the Godambe Information Matrix, to
+# account for linkage in the data. To use the GIM approach, we need to have
+# spectra from bootstrapping our data.  Let's load the ones we've provided for
+# the example.  
 # (We're using Python list comprehension syntax to do this in one line.)
 all_boot = [dadi.Spectrum.from_file('bootstraps/{0:02d}.fs'.format(ii)) 
             for ii in range(100)]
+uncerts = dadi.Godambe.GIM_uncert(func_ex, pts_l, all_boot, popt, data, 
+                                  multinom=True)
+# uncert contains the estimated standard deviations of each parameter, with
+# theta as the final entry in the list.
+print('Estimated parameter standard deviations from GIM: {0}'.format(uncerts))
 
-uncert = dadi.godambe.uncert(func_ex, pts_l, all_boot, popt, data, 0.01, 
-                             log=True, multinom=True)
-print uncert, " are uncertainties from GIM"
+# For comparison, we can estimate uncertainties with the Fisher Information
+# Matrix, which doesn't account for linkage in the data and thus underestimates
+# uncertainty. (Although it's a fine approach if you think your data is truly
+# unlinked.)
+uncerts_fim = dadi.Godambe.FIM_uncert(func_ex, pts_l, popt, data, multinom=True)
+print('Estimated parameter standard deviations from FIM: {0}'.format(uncerts_fim))
+
+print('Factors by which FIM underestimates parameter uncertainties: {0}'.format(uncerts/uncerts_fim))
+
+# What if we fold the data?
+# These are the optimal parameters when the spectrum is folded. They can be
+# found simply by passing data.fold() to the above call to optimize_log. 
+popt_fold =  array([1.907,  0.073,  1.830,  0.899,  0.425,  0.113])
+uncerts_folded = dadi.Godambe.GIM_uncert(func_ex, pts_l, all_boot, popt_fold, 
+                                         data.fold(), multinom=True)
+print('Folding increases parameter uncertainties by factors of: {0}'.format(uncerts_folded/uncerts))
 
 # Let's do a likelihood-ratio test comparing models with and without migration.
 # The no migration model is implemented as 
 # demographic_models.prior_onegrow_nomig
-# The best-fit ll for this model is -1146.09.
-# These are the best-fit models parameters
-# array([ 1.897,  0.0388,  9.677,  0.395,  0.070]).
-# Since LRT evaluates the complex model using the
-# best-fit parameters from the simple model,
-# need to create list of parameters for the complex
-# model using the simple (no-mig) best-fit params
-# Also, calculating theta to maintain consistency
-# with uncert example above.
-p0 = [1.897,  0.0388,  9.677, 0.395,  0.070]
-# Since evalution is done with more complex model,
-# need to insert zero migration value at corresponding
-# migration parameter index in complex model
-p0.insert(3, 0.0)
+func_nomig = demographic_models.prior_onegrow_nomig
+func_ex_nomig = dadi.Numerics.make_extrap_log_func(func_nomig)
+# These are the best-fit parameters, which we found by multiple optimizations
+popt_nomig = array([ 1.897,  0.0388,  9.677,  0.395,  0.070])
+model_nomig = func_ex_nomig(popt_nomig, ns, pts_l)
+ll_nomig = dadi.Inference.ll_multinom(model_nomig, data)
 
-LRT = dadi.godambe.LRT(func_ex, pts_l, all_boot, p0, data, 0.01, diff_indices=[3], multinom=True)
-print LRT, " is the value by which to adjust the LRT test statistic"
-print "This yields a test statistic of ", LRT*2*(ll_model-(-1146.09))
+# Since LRT evaluates the complex model using the best-fit parameters from the
+# simple model, we need to create list of parameters for the complex model
+# using the simple (no-mig) best-fit params.  Since evalution is done with more
+# complex model, need to insert zero migration value at corresponding migration
+# parameter index in complex model. And we need to tell the LRT adjust function
+# that the 3rd parameter (counting from 0) is the nested one.
+p_lrt = [1.897,  0.0388,  9.677, 0, 0.395,  0.070]
+
+adj = dadi.Godambe.LRT_adjust(func_ex, pts_l, all_boot, p_lrt, data, 
+                              diff_indices=[3], multinom=True)
+D_adj = adj*2*(ll_model - ll_nomig)
+print('Adjusted D statistic: {0:.4f}'.format(D_adj))
+
+# Because this is test of a parameter on the boundary of parameter space 
+# (m cannot be less than zero), our null distribution is an even proportion 
+# of chi^2 distributions with 0 and 1 d.o.f. To evaluate the p-value, we use the
+# point percent function for a weighted sum of chi^2 dists.
+pval = dadi.Godambe.sum_chi2_ppf(D_adj, weights=(0.5,0.5))
+print('p-value for rejecting no-migration model: {0:.4f}'.format(pval))
