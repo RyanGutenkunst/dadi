afe82f2fb855fe79f147c667ae4fecb0a07d86bb
Kun Wang <wk8910@gmail.com>
2016-12-13 22:42:50 +0800

    type error in Demographics2D: Tpre -> TPre

--- dadi/Demographics2D.py
+++ dadi/Demographics2D.py
@@ -226,7 +226,7 @@ def IM_pre_mscore(params):
     """
     ms core command for IM_pre.
     """
-    nuPre,Tpre,s,nu1,nu2,T,m12,m21 = params
+    nuPre,TPre,s,nu1,nu2,T,m12,m21 = params
     
     nu1_0 = nuPre*s
     nu2_0 = nuPre*(1-s)
