490f89c090dacdb82c461a34bc084e7b4ca7a780
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2017-04-21 11:57:35 -0700

    Clean up two_epoch and three_epoch models, to use simpler equilibration code.

--- dadi/TwoLocus/demographics.py
+++ dadi/TwoLocus/demographics.py
@@ -24,8 +24,6 @@ def equilibrium_phi(pts, rho, dt, gammaA=0, gammaB=0, hA=0, hB=0,
     except IOError:
         print('Calculating equilibrium phi from scratch.')
         x = numerics.grid(pts)
-        dx = numerics.grid_dx(x)
-        dx3 = numerics.grid_dx3(x,dx)
 
         yA = dadi.PhiManip.phi_1D(x, gamma = gammaA, h = hA, theta0 = thetaA)
         yB = dadi.PhiManip.phi_1D(x, gamma = gammaB, h = hB, theta0 = thetaB)
@@ -49,63 +47,43 @@ def equilibrium(pts, ns, rho=0.0, dt=0.005, gammaA=0.0, gammaB=0.0,
     fs = numerics.sample_cached(phi, ns, x, dx3)
     return fs
 
-def two_epoch(params, numpts, ns, rho=0.0, dt=0.005, gammaA = 0.0, gammaB = 0.0, hA = 0.5, hB = 0.5):
-    rho = float(rho); gammaA = float(gammaA); gammaB = float(gammaB)
+def two_epoch(params, pts, ns, rho=0.0, dt=0.005, gammaA=0.0, gammaB=0.0,
+              hA=0.5, hB=0.5, thetaA=1.0, thetaB=1.0):
     nu,T = params
-    x = numerics.grid(numpts)
-    dx = numerics.grid_dx(x)
-    U01 = numerics.domain(x)
-    dx3 = numerics.grid_dx3(x,dx)
 
-    eq_fname = cache_path + 'phis_pts{0}_dt{1}_rho{2}_gammaA{3}_gammaB{4}.npz'.format(numpts,dt,rho,gammaA,gammaB)
-    try:
-        open_file = np.load(eq_fname)
-        #print 'using cached spectra from equilibrium'
-        yA,yB,phi = open_file['arr_0'], open_file['arr_1'], open_file['arr_2']
-        open_file.close()
-    except IOError:
-        print 'calculating equilibrium from scratch for [{0},{1}]'.format(numpts,dt)
-        yA = dadi.PhiManip.phi_1D(x)  ## account for gamma!!!
-        yB = dadi.PhiManip.phi_1D(x)  ## account for gamma!!!
-        phi = np.zeros((len(x),len(x),len(x)))
-        yA,yB,phi = integration.advance(phi, x, 20., yA, yB, gammaA=gammaA, gammaB=gammaB, hA=hA, hB=hB, rho=rho, thetaA=1., thetaB=1., dt=dt)
-        np.savez(eq_fname,yA,yB,phi)
-    
-    if T > 0:
-        yA,yB,phi = integration.advance(phi, x, T, yA, yB, nu=nu, gammaA=gammaA, gammaB=gammaB, hA=hA, hB=hB, rho=rho, thetaA=1.0, thetaB=1.0, dt=dt)
-    
+    x = numerics.grid(pts)
+    dx = numerics.grid_dx(x)
+    dx3 = numerics.grid_dx3(x,dx)
+    yA, yB, phi = equilibrium_phi(pts, rho, dt, gammaA, gammaB, hA, hB,
+                                  thetaA, thetaB)
+
+    yA,yB,phi = integration.advance(phi, x, T, yA, yB, nu=nu,
+                                    gammaA=gammaA, gammaB=gammaB,
+                                    hA=hA, hB=hB, rho=rho,
+                                    thetaA=thetaA, thetaB=thetaB, dt=dt)
+
     fs = numerics.sample_cached(phi, ns, x, dx3)
     return fs
 
-
-
-def three_epoch(params, numpts, ns, rho=0.0, dt=0.005, gammaA = 0.0, gammaB = 0.0, hA = 0.5, hB = 0.5):
-    rho = float(rho); gammaA = float(gammaA); gammaB = float(gammaB)
+def three_epoch(params, pts, ns, rho=0.0, dt=0.005, gammaA=0.0, gammaB=0.0,
+                hA=0.5, hB=0.5, thetaA=1.0, thetaB=1.0):
     nu1,nu2,T1,T2 = params
-    x = numerics.grid(numpts)
+    x = numerics.grid(pts)
     dx = numerics.grid_dx(x)
-    U01 = numerics.domain(x)
     dx3 = numerics.grid_dx3(x,dx)
 
-    eq_fname = cache_path + 'phis_pts{0}_dt{1}_rho{2}_gammaA{3}_gammaB{4}.npz'.format(numpts,dt,rho,gammaA,gammaB)
-    try:
-        open_file = np.load(eq_fname)
-        #print 'using cached spectra from equilibrium'
-        yA,yB,phi = open_file['arr_0'], open_file['arr_1'], open_file['arr_2']
-        open_file.close()
-    except IOError:
-        print 'calculating equilibrium from scratch'
-        yA = dadi.PhiManip.phi_1D(x)  ## account for gamma!!!
-        yB = dadi.PhiManip.phi_1D(x)  ## account for gamma!!!
-        phi = np.zeros((len(x),len(x),len(x)))
-        yA,yB,phi = integration.advance(phi, x, 20., yA, yB, gammaA=gammaA, gammaB=gammaB, hA=hA, hB=hB, rho=rho, thetaA=1.0, thetaB=1.0, dt=dt)
-        np.savez(eq_fname,yA,yB,phi)
-    
-    if T1 > 0:
-        yA,yB,phi = integration.advance(phi, x, T1, yA, yB, nu=nu1, gammaA=gammaA, gammaB=gammaB, hA=hA, hB=hB, rho=rho, thetaA=1.0, thetaB=1.0, dt=dt)
-    
-    if T2 > 0:
-        yA,yB,phi = integration.advance(phi, x, T2, yA, yB, nu=nu2, gammaA=gammaA, gammaB=gammaB, hA=hA, hB=hB, rho=rho, thetaA=1.0, thetaB=1.0, dt=dt)
-    
+    yA, yB, phi = equilibrium_phi(pts, rho, dt, gammaA, gammaB, hA, hB,
+                                  thetaA, thetaB)
+
+    yA,yB,phi = integration.advance(phi, x, T1, yA, yB, nu=nu1,
+                                    gammaA=gammaA, gammaB=gammaB,
+                                    hA=hA, hB=hB, rho=rho,
+                                    thetaA=thetaA, thetaB=thetaB, dt=dt)
+
+    yA,yB,phi = integration.advance(phi, x, T2, yA, yB, nu=nu2,
+                                    gammaA=gammaA, gammaB=gammaB,
+                                    hA=hA, hB=hB, rho=rho,
+                                    thetaA=thetaA, thetaB=thetaB, dt=dt)
+
     fs = numerics.sample_cached(phi, ns, x, dx3)
     return fs
