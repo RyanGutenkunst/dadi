0e4e9cf05573ac4f01344eace7e883bceb74199b
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2017-04-21 11:50:52 -0700

    Simplify handling of equilibrum cache in TwoLocus demographics

--- dadi/TwoLocus/demographics.py
+++ dadi/TwoLocus/demographics.py
@@ -10,26 +10,41 @@ cache_path = os.path.join(os.path.expanduser('~'), '.dadi',
 if not os.path.isdir(cache_path):
     os.makedirs(cache_path)
 
-
-def equilibrium(numpts, ns, rho=0.0, dt=0.005, gammaA = 0.0, gammaB = 0.0, hA = 0.5, hB = 0.5):
-    rho = float(rho); gammaA = float(gammaA); gammaB = float(gammaB)
-    eq_fname = cache_path + 'phis_pts{0}_dt{1}_rho{2}_gammaA{3}_gammaB{4}.npz'.format(numpts,dt,rho,gammaA,gammaB)
-    x = numerics.grid(numpts)
-    dx = numerics.grid_dx(x)
-    U01 = numerics.domain(x)
-    dx3 = numerics.grid_dx3(x,dx)
+def equilibrium_phi(pts, rho, dt, gammaA=0, gammaB=0, hA=0, hB=0,
+                    thetaA=1.0, thetaB=1.0):
+    rho=float(rho); gammaA=float(gammaA); gammaB=float(gammaB);
+    hA=float(hA); hB=float(hB); thetaA=float(thetaA); thetaB=float(thetaB)
+    fname_template = 'phis_pts{0}_dt{1}_rho{2}_gammaA{3}_gammaB{4}_hA{5}_hB{6}_thetaA{7}_thetaB{8}.npz'
+    fname = fname_template.format(pts,rho,dt,gammaA,gammaB,hA,hB,thetaA,thetaB)
+    fname = os.path.join(cache_path, fname)
     try:
-        open_file = np.load(eq_fname)
-        #print 'using cached spectra from equilibrium'
-        yA,yB,phi = open_file['arr_0'], open_file['arr_1'], open_file['arr_2']
-        open_file.close()
+        fid = np.load(fname)
+        yA,yB,phi = fid['arr_0'], fid['arr_1'], fid['arr_2']
+        fid.close()
     except IOError:
-        print 'calculating equilibrium from scratch'
-        yA = dadi.PhiManip.phi_1D(x,gamma=gammaA)
-        yB = dadi.PhiManip.phi_1D(x,gamma=gammaB)
+        print('Calculating equilibrium phi from scratch.')
+        x = numerics.grid(pts)
+        dx = numerics.grid_dx(x)
+        dx3 = numerics.grid_dx3(x,dx)
+
+        yA = dadi.PhiManip.phi_1D(x, gamma = gammaA, h = hA, theta0 = thetaA)
+        yB = dadi.PhiManip.phi_1D(x, gamma = gammaB, h = hB, theta0 = thetaB)
+
         phi = np.zeros((len(x),len(x),len(x)))
-        yA,yB,phi = integration.advance(phi, x, 20., yA, yB, gammaA=gammaA, gammaB=gammaB, hA=hA, hB=hB, rho=rho, thetaA=1., thetaB=1., dt=dt)
-        np.savez(eq_fname,yA,yB,phi)
+        yA, yB, phi = integration.advance(phi, x, 20., yA, yB, 1.0,
+                                          gammaA, gammaB, hA, hB, rho,
+                                          thetaA, thetaB, dt)
+        np.savez(fname, yA, yB, phi)
+    return yA, yB, phi
+
+def equilibrium(pts, ns, rho=0.0, dt=0.005, gammaA=0.0, gammaB=0.0,
+                 hA=0.5, hB=0.5, thetaA=1.0, thetaB=1.0):
+    x = numerics.grid(pts)
+    dx = numerics.grid_dx(x)
+    dx3 = numerics.grid_dx3(x,dx)
+
+    yA, yB, phi = equilibrium_phi(pts, rho, dt, gammaA, gammaB, hA, hB,
+                                  thetaA, thetaB)
 
     fs = numerics.sample_cached(phi, ns, x, dx3)
     return fs
