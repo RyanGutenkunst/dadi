4666f7c419e54486e85b333b1b88f561032f6246
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2018-11-03 13:38:21 -0700

    Cache at higher level in Godambe.py, for greater efficiency when uncertainties are being calculated repeatedly.

--- dadi/Godambe.py
+++ dadi/Godambe.py
@@ -177,6 +177,7 @@ def get_grad(func, p0, eps, args=()):
             grad[ii] = (fp - fm)/eps[ii]
     return grad
 
+cache = {}
 @print_citation
 def get_godambe(func_ex, grid_pts, all_boot, p0, data, eps, log=False,
                 just_hess=False, boot_theta_adjusts=[]):
@@ -203,7 +204,6 @@ def get_godambe(func_ex, grid_pts, all_boot, p0, data, eps, log=False,
 
     # Cache evaluations of the frequency spectrum inside our hessian/J 
     # evaluation function
-    cache = {}
     def func(params, data, theta_adjust=1):
         key = (tuple(params), tuple(ns), tuple(grid_pts))
         if key not in cache:
