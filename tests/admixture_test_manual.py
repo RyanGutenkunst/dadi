66753ae1c415319e8b009683268703bc706454cd
Ryan Gutenkunst <rgutenk@email.arizona.edu>
2019-03-26 21:24:57 -0700

    Convert admixture test for ms comparison

--- tests/admixture_test_manual.py
+++ tests/admixture_test_manual.py
@@ -2,7 +2,7 @@
 Test script for validating performance of different simulation techniques
 when admixture is present.
 """
-import cPickle, os, sys, time
+import pickle, os, sys, time
 import numpy
 from numpy import array
 import matplotlib.pyplot as pyplot
@@ -11,8 +11,8 @@ import dadi
 REFRESH_DADI = True
 
 try:
-    dadi_cache = cPickle.load(file('dadi_cache.bp'))
-    ms_cache = cPickle.load(file('ms_cache.bp'))
+    dadi_cache = pickle.load(open('dadi_cache.bp', 'rb'))
+    ms_cache = pickle.load(open('ms_cache.bp', 'rb'))
 except IOError:
     dadi_cache = {}
     ms_cache = {}
@@ -38,10 +38,11 @@ def demo_func(params, ns, pts, crwd=8, method='PhiManip'):
 
     return fs
 
-def ms_corefunc((nuC, nu1, nu2, nu1B, nu1F, Tc, Ts, Tb, m12, m21, f, Tf)):
-	alpha1 = numpy.log(nu1F/nu1B)/Tb
+def ms_corefunc(params):
+    (nuC, nu1, nu2, nu1B, nu1F, Tc, Ts, Tb, m12, m21, f, Tf) = params
+    alpha1 = numpy.log(nu1F/nu1B)/Tb
 	
-	command = '-n 1 %(nu1F)f -n 2 %(nu2)f '\
+    command = '-n 1 %(nu1F)f -n 2 %(nu2)f '\
 		'-es %(Tf)f 1 %(admix)f -ej %(Tf)f 3 2 '\
 		'-ma x %(m12)f %(m21)f x '\
 		'-eg %(Tf)f 1 %(alpha1)f '\
@@ -49,15 +50,15 @@ def ms_corefunc((nuC, nu1, nu2, nu1B, nu1F, Tc, Ts, Tb, m12, m21, f, Tf)):
 		'-en %(Ts)f 1 %(nuC)f -ej %(Ts)f 2 1 '\
 		'-en %(Tsum)f 1 1'
 
-        sub_dict = { 'nuC':nuC, 'nu1':nu1, 'nu2':nu2, 'nu1B':nu1B, 'nu1F':nu1F, 'Tf':Tf/2, 'Tb':(Tb+Tf)/2, 'Ts':((Tb+Ts+Tf)/2), 'Tsum':((Tc+Ts+Tb+Tf)/2), 'alpha1':2*alpha1, 'm12':2*m12, 'm21':2*m21, 'admix':1-f }
-	return command % sub_dict
+    sub_dict = { 'nuC':nuC, 'nu1':nu1, 'nu2':nu2, 'nu1B':nu1B, 'nu1F':nu1F, 'Tf':Tf/2, 'Tb':(Tb+Tf)/2, 'Ts':((Tb+Ts+Tf)/2), 'Tsum':((Tc+Ts+Tb+Tf)/2), 'alpha1':2*alpha1, 'm12':2*m12, 'm21':2*m21, 'admix':1-f }
+    return command % sub_dict
 
 def cached_demo_func(params, ns, pts, crwd=8, method='PhiManip'):
     key = (tuple(params), ns, pts, crwd, method)
     if REFRESH_DADI or not dadi_cache.has_key(key):
         start = time.time()
         dadi_cache[key] = demo_func(params, ns, pts, crwd=crwd, method=method)
-        print 'Time for dadi, pts:', time.time() - start, pts
+        print('Time for dadi, pts:', time.time() - start, pts)
         sys.stdout.flush()
     return dadi_cache[key]
 
@@ -71,7 +72,7 @@ def cached_ms_run(params, ns, theta_tot):
         result = dadi.Spectrum.from_ms_file(os.popen(mscommand), 
                                             pop_ids=['BIA','YRI'],
                                             average=False)
-        print 'Time for ms:', time.time() - start
+        print('Time for ms:', time.time() - start)
         sys.stdout.flush()
         ms_cache[key] = (theta_tot, result)
     else:
@@ -101,8 +102,8 @@ msdata, theta_tot = cached_ms_run(params, ns, 1e5)
 model *= theta_tot
 ll_model = dadi.Inference.ll(model, msdata)
 theta = dadi.Inference.optimal_sfs_scaling(model, msdata)
-print 'LL:', ll_model
-print 'theta scaling:', theta
+print('LL:', ll_model)
+print('theta scaling:', theta)
 
 dadi.Plotting.plot_2d_comp_Poisson(model, msdata, vmin=1, fig_num=1)
 fig = pyplot.gcf()
@@ -135,7 +136,7 @@ ax.set_title('Entry [bia={0:d}, yri={1:d}]'.format(bia_ii, yri_jj))
 
 lls = [dadi.Inference.ll(theta_tot*m, msdata) for m in all_fs]
 for pts, ll in zip(all_pts, lls):
-    print pts, ll
+    print(pts, ll)
 
 ###############################################################################
 #
@@ -143,11 +144,11 @@ for pts, ll in zip(all_pts, lls):
 #
 ###############################################################################
 
-fid = file('ms_cache.bp', 'w')
-cPickle.dump(ms_cache, fid, 2)
+fid = open('ms_cache.bp', 'wb')
+pickle.dump(ms_cache, fid)
 fid.close()
-fid = file('dadi_cache.bp', 'w')
-cPickle.dump(dadi_cache, fid, 2)
+fid = open('dadi_cache.bp', 'wb')
+pickle.dump(dadi_cache, fid)
 fid.close()
 
-pyplot.show()
+#pyplot.show()
