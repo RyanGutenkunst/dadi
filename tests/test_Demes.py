eb4749004a3fcbcefd08e0092155069cb5d72b46
tjstruck <tjstruck@email.arizona.edu>
2021-06-22 14:31:19 -0700

    Update test_Demes.py, added Admixure/Pulse tests, updated comparison with an OOA that has the Phi reordering that is done in Demes

--- tests/test_Demes.py
+++ tests/test_Demes.py
@@ -44,16 +44,91 @@ class TestSplits(unittest.TestCase):
         for props, parent in zip(proportions, pop_ids):
             dadi_phi5D = dadi.PhiManip.phi_4D_to_5D(dadi_phi4D, props[0], props[1], props[2], xx,xx,xx,xx,xx)
             demes_phi5D = Demes._split_phi(dadi_phi4D, xx, pop_ids, parent)
-            self.assertTrue(np.allclose(demes_phi4D, dadi_phi4D))
+            self.assertTrue(np.allclose(demes_phi5D, dadi_phi5D))
+
+class TestIntegration(unittest.TestCase):
+    def test_integration(self):
+        xx = dadi.Numerics.default_grid(pts)
+        phi1D = dadi.PhiManip.phi_1D(xx)
+
+        integration_params = nu, T, M, gamma, h, theta, frozen = [
+        [1.685],0.22, [], [0], [0.5], 1, [False]
+        ]
+        pop_ids = ['1']
+
+        dadi_phi1D = dadi.Integration.one_pop(phi1D, xx, T, nu[0])
+        demes_phi1D = Demes._integrate_phi(phi1D, xx, integration_params, pop_ids)
+
+        integration_params = nu, T, M, gamma, h, theta, frozen = [
+        [1.685,0.29],0.325, np.array([[0,3.65],[3.65,0]]), [0,0], [0.5,0.5], 1, [False,False]
+        ]
+        pop_ids = ['1','2']
+        phi2D = dadi.PhiManip.phi_1D_to_2D(xx, dadi_phi1D)
+
+        dadi_phi2D = dadi.Integration.two_pops(phi2D, xx, T, nu[0], nu[1], m12=M[0,1], m21=M[1,0])
+        demes_phi2D = Demes._integrate_phi(phi2D, xx, integration_params, pop_ids)
+        self.assertTrue(np.allclose(demes_phi2D, dadi_phi2D))
+
+        integration_params = nu, T, M, gamma, h, theta, frozen = [
+        [2,1,3],1, np.array([[0,1,2],[1,0,2],[2,2,0]]), 
+        [0,0,0], [0.5,0.5,0.5], 1, [False,False,False]
+        ]
+        pop_ids = ['1','2','3']
+        phi3D = dadi.PhiManip.phi_2D_to_3D_split_2(xx, dadi_phi2D)
+
+        dadi_phi3D = dadi.Integration.three_pops(phi3D, xx, T, nu[0], nu[1], nu[2], 
+        	m12=M[0,1], m13=M[0,2], m21=M[1,0], m23=M[1,2], m31=M[2,0], m32=M[2,1])
+        demes_phi3D = Demes._integrate_phi(phi3D, xx, integration_params, pop_ids)
+        self.assertTrue(np.allclose(demes_phi3D, dadi_phi3D))
+
+class TestAdmix(unittest.TestCase):
+        def test_admix(self):
+            xx = dadi.Numerics.default_grid(pts)
+            phi1D = dadi.PhiManip.phi_1D(xx)
+
+            dadi_phi2D = dadi.PhiManip.phi_1D_to_2D(xx, phi1D)
+
+            pop_ids = ['1']
+            parent = '1'
+            children=['A', 'B']
+            demes_phi2D = Demes._split_phi(phi1D, xx, pop_ids, parent)
+
+            proportions, pop_ids = [0.7, children]
+            for sources, dest in zip(pop_ids, pop_ids[::-1]):
+                i = pop_ids.index(dest)
+                phifunc = [
+                dadi.PhiManip.phi_2D_admix_2_into_1,
+                dadi.PhiManip.phi_2D_admix_1_into_2
+                ][i]
+                dadi_phi2D_admix = phifunc(dadi_phi2D, proportions, xx,xx)
+                demes_phi2D_admix = Demes._admix_phi(demes_phi2D, xx, proportions, pop_ids, sources, dest)
+                self.assertTrue(np.allclose(demes_phi2D_admix, dadi_phi2D_admix))
+        def test_admix_new_pop(self):
+            xx = dadi.Numerics.default_grid(pts)
+            phi1D = dadi.PhiManip.phi_1D(xx)
+
+            dadi_phi2D = dadi.PhiManip.phi_1D_to_2D(xx, phi1D)
+
+            pop_ids = ['1']
+            parent = '1'
+            children=['A', 'B']
+            demes_phi2D = Demes._split_phi(phi1D, xx, pop_ids, parent)
+
+            proportions, pop_ids, parents = [[0.7, 0.3], children, children]
+            dadi_phi3D = dadi.PhiManip.phi_2D_to_3D_admix(dadi_phi2D, proportions[0], xx,xx,xx)
+            demes_phi3D = Demes._admix_new_pop_phi(demes_phi2D, xx, proportions, pop_ids, parents)
+            self.assertTrue(np.allclose(demes_phi3D, dadi_phi3D))
 
 class DemesDataTestCase(unittest.TestCase):
     def test_basic_loading(self):
         fs_demes = dadi.Spectrum.from_demes(model, sampled_demes=sampled_demes, sample_sizes=sample_sizes, pts=pts_l)
         self.assertTrue(np.allclose(fs_demes[2,5,6], 0.0025822560555528017))
         self.assertTrue(np.allclose(fs_demes[6,3,2], 0.0017156293580770643))
+
     def test_demes_vs_dadi(self):
         fs_demes = dadi.Spectrum.from_demes(model, sampled_demes=sampled_demes, sample_sizes=sample_sizes, pts=pts_l)
-        def OutOfAfrica(params, ns, pts):
+        
+        def OutOfAfrica_with_demes_reordering(params, ns, pts):
             nuAf, nuB, nuEu0, nuEu, nuAs0, nuAs, mAfB, mAfEu, mAfAs, mEuAs, TAf, TB, TEuAs = params
             xx = dadi.Numerics.default_grid(pts)
 
@@ -61,13 +136,24 @@ class DemesDataTestCase(unittest.TestCase):
             phi = dadi.Integration.one_pop(phi, xx, TAf, nu = nuAf)
 
             phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
-            phi = dadi.Integration.two_pops(phi, xx, TB, nu1 = nuAf, nu2 = nuB, m12 = mAfB, m21 = mAfB)
+            #Reorder after first split
+            phi = dadi.PhiManip.reorder_pops(phi,[2,1])
 
-            phi = dadi.PhiManip.phi_2D_to_3D_split_2(xx, phi)
+            # 2D Integration
+            # OOA is first pop, so now nu1=nuB and nu2=nuAf
+            phi = dadi.Integration.two_pops(phi, xx, TB, nu1 = nuB, nu2 = nuAf, m12 = mAfB, m21 = mAfB)
+
+            # OOA is now the first population, so you use phi_2D_to_3D_split_1
+            # instead of phi_2D_to_3D_split_2
+            phi = dadi.PhiManip.phi_2D_to_3D_split_1(xx, phi)
+
+            # Because YRI should be the first pop, we need to reorder
+            # so that it is the first population
+            phi = dadi.PhiManip.reorder_pops(phi,[2,1,3])
 
             nuEu_func = lambda t : nuEu0 * (nuEu/nuEu0) ** (t/TEuAs)
             nuAs_func = lambda t : nuAs0 * (nuAs/nuAs0) ** (t/TEuAs)
-            phi = dadi.Integration.three_pops(phi, xx, TEuAs, nu1 = nuAf, nu2 = nuEu_func, nu3 = nuAs_func, 
+            phi = dadi.Integration.three_pops(phi, xx, TEuAs, nu1 = nuAf, nu2 = nuEu_func, nu3 = nuAs_func,
                 m12 = mAfEu, m13 = mAfAs, m21 = mAfEu, m23 = mEuAs, m31 = mAfAs, m32 = mEuAs)
 
             fs = dadi.Spectrum.from_phi(phi, ns, (xx, xx, xx))
@@ -88,30 +174,18 @@ class DemesDataTestCase(unittest.TestCase):
         mAfEu = 2 * Ne * 3e-5
         mAfAs = 2 * Ne * 1.9e-5
         mEuAs = 2 * Ne * 9.6e-5
+
         params = [
         nuA, nuB, nuEu0, nuEuF, nuAs0, nuAsF, mAfB, mAfEu, mAfAs, mEuAs, TA, TB, TF
         ]
-        # params = [1.6849315068493151, 0.2876712328767123, 
-        # 0.136986301369863, 4.071917808219178, 0.06986301369863014, 7.409589041095891, 
-        # 3.65, 0.438, 0.2774, 1.4016,
-        # 0.2191780821917808, 0.3254794520547945, 0.05808219178082192]
 
-        func_ex = dadi.Numerics.make_extrap_func(OutOfAfrica)
-
+        func_ex = dadi.Numerics.make_extrap_func(OutOfAfrica_with_demes_reordering)
         fs_dadi = func_ex(params, sample_sizes, pts_l)
-        # fs_demes.pop_ids=None
 
-        l = []
-        for i in range(1,len(fs_demes)-1):
-            for ii in range(1,len(fs_demes[i])-1):
-                for iii in range(1,len(fs_demes[i][ii])-1):
-                    if np.allclose(fs_demes[i,ii,iii], fs_dadi[i,ii,iii]):
-                        l.append([i,ii,iii])
+        self.assertTrue(np.allclose(fs_demes, fs_dadi))
+        #self.assertTrue(np.allclose(fs_demes[6,3,2], fs_demes[6,3,2]))
 
-        self.assertTrue(np.allclose(fs_demes[2,5,6], fs_dadi[2,5,6]))
-        self.assertTrue(np.allclose(fs_demes[6,3,2], fs_demes[6,3,2]))
-
-suite=unittest.TestLoader().loadTestsFromTestCase(TestSplits)
+suite=unittest.TestLoader().loadTestsFromTestCase(TestAdmix)
 
 if __name__ == '__main__':
     unittest.main()
